5a4fab8e92808a45916cdd6358b84e1968801b59
fkjaekel <fkjaekel@gmail.com>
2019-12-26 11:08:36 -0300

    Add files via upload

---
+++ ConstSPD.pas
@@ -0,0 +1,33 @@
+//----------------------------------------------------------------------------//
+//DESENVOLVEDOR : JOÃO PAULO DA SILVA SIMÕES                                  //
+//----------------------------------------------------------------------------//
+//DATAS                                                                       //
+//INICIO           : 05/03/2012                                               //
+//TERMINO          : 05/03/2012                                               //
+//LIBERAÇÃO TESTES : 06/03/2012                                               //
+//----------------------------------------------------------------------------//
+//P.S :                                                                       //
+//----------------------------------------------------------------------------//
+
+unit ConstSPD;
+
+interface
+
+uses
+  SGHTiposEnumerados;
+
+type
+  TEstadoFormulario = (tefPronto, tefEspera);
+
+const
+  VERSAO_SISTEMA : String         = '19.02';
+  versaoSistema  : TTipoVersaoSGH = ttvs1902;
+  CRIPT_KEY      : String         = 'P@ssw0rd#260788';
+
+implementation
+
+
+
+
+end.
+ 
